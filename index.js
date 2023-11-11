179bfb1fdf0810a1e07e09a6edcfd543d1711b18
Miraj Howlader <110389768+MirajHowlader@users.noreply.github.com>
2023-11-12 01:16:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const inputEl = document.getElementById('input');
+const infoText = document.getElementById('info-text');
+const meaningContainer = document.getElementById('meaning-container');
+const titleEl = document.getElementById('title');
+const meaningEl = document.getElementById('meaing');
+const audioEl = document.getElementById('audio');
+
+
+
+async function fetchAPI(word){
+   try {
+    infoText.style.display = 'block';
+    meaningContainer.style.display='none';
+
+
+    infoText.innerText = `Searching the meaning of "${word}"`
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    const result = await fetch(url).then((res)=>res.json());
+
+    if(result.title){
+        meaningContainer.style.display='block';
+        titleEl.innerText = word;
+        meaningEl.innerText  = 'N?A';
+        audioEl.style.display = 'none';
+    }else{
+
+    infoText.style.display = 'none';
+    meaningContainer.style.display='block';
+    audioEl.style.display = 'inline-flex'
+    titleEl.innerText = result[0].word;
+    meaningEl.innerText  = result[0].meanings[0].definitions[0].definition;
+
+    audioEl.src = result[0].phonetics[0].audio;
+    }
+    
+   } catch (error) {
+    infoText.innerText = `an error happened try again later`
+    
+   }
+
+    
+}
+
+
+
+inputEl.addEventListener('keyup', (e) =>{
+    if(e.target.value && e.key === 'Enter'){
+        fetchAPI(e.target.value)
+    }
+})
